a81126f7bda76fca2d99c140b4bc6f34847d6179
Inna Zilberov <innaz@Innas-MBP.fritz.box>
2020-10-27 11:31:57 +0100

    some formating of the code

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ function App() {
     <Router>
     <div className="App">
       <Route component={HomePage} exact path="/"/>
-     <Route component={Market} path= "/market"/>
-     <Route component={HowMuch} path= "/howmuch"/>
-     <Route component={Test} path= "/taketest"/>
-     <Route component={Type1} path= "/type1"/>
-     <Route component={Type2} path= "/type2"/>
-     <Route component={Type3} path= "/type3"/>
-     <Route component={Portfolio} path= "/portfolio"/>
-     <Route component={Buy} path= "/buy"/>
-     <Route component={Sell} path= "/sell"/>
-
+      <Route component={Market} path= "/market"/>
+      <Route component={HowMuch} path= "/howmuch"/>
+      <Route component={Test} path= "/taketest"/>
+      <Route component={Type1} path= "/type1"/>
+      <Route component={Type2} path= "/type2"/>
+      <Route component={Type3} path= "/type3"/>
+      <Route component={Portfolio} path= "/portfolio"/>
+      <Route component={Buy} path= "/buy"/>
+      <Route component={Sell} path= "/sell"/>
     </div>
     </Router>
   );
@@ -48,51 +47,25 @@ function HomePage(){
 
   return(
     <div>
-
-<h1>Welcome to CryptoSim</h1>
-<Link to= "/howmuch"><button>Start simulation</button></Link>
-
-
+      <h1>Welcome to CryptoSim</h1>
+      <Link to= "/howmuch"><button>Start simulation</button></Link>
     </div>
-
-
   )
-
 };
 
 
-// function Howmuch(){
-
-//   return(
-//     <div>
-//       <form>
-//   <label>
-//     How much would you like to invest?
-//     <p>Text explaining the steplorem ipsum dolor sit amet, consectetur adipiscing </p>
-//     <input type="text" name="name" />
-//   </label>
-//   <Link to= "/taketest"><input type="submit" value="Submit" /></Link>
-//   </form>
-//   </div>
-//   )
-// };
 
 function Test(){
 
   return(
     <div>
       <h1>Here you can take the test to find out what kind of investor are you </h1>
-
-<button> Take test</button>
-
-<h2>If you already know, click one the one</h2>
-<Link to = "/type1"><p>Type1</p></Link>
-<Link to = "/type2"><p>Type2</p></Link>
-<Link to = "/type3"><p>Type3</p></Link>
-
+      <button> Take test</button>
+      <h2>If you already know, click one the one</h2>
+      <Link to = "/type1"><p>Type1</p></Link>
+      <Link to = "/type2"><p>Type2</p></Link>
+      <Link to = "/type3"><p>Type3</p></Link>
     </div>
-
-
   )
 
 };
