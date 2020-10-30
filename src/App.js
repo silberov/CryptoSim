e506ba1832fe6c7ac91135fd5971647a12f4cd65
Inna Zilberov <innaz@Innas-MBP.fritz.box>
2020-10-30 15:31:57 +0100

    last

--- src/App.js
+++ src/App.js
@@ -10,16 +10,7 @@ function App() {
     <div className="App">
       <HowMuch />
       <Test />
-      {/* <Route component={HomePage} exact path="/"/>
-      <Route component={Market} path= "/market"/>
-      <Route component={HowMuch} path= "/howmuch"/>
-      <Route component={Test} path= "/taketest"/>
-      <Route component={Type1} path= "/type1"/>
-      <Route component={Type2} path= "/type2"/>
-      <Route component={Type3} path= "/type3"/>
-      <Route component={Portfolio} path= "/portfolio"/>
-      <Route component={Buy} path= "/buy"/>
-      <Route component={Sell} path= "/sell"/> */}
+ 
     </div>
 
   );
