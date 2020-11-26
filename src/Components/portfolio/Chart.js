a1c2829a7786c1ddac3d2aa33c139d70616bf827
Your Name <you@example.com>
2020-11-26 09:49:30 +0100

    CSS changes

--- src/Components/portfolio/Chart.js
+++ src/Components/portfolio/Chart.js
@@ -10,19 +10,45 @@
 
 import React from 'react';
 import {Line} from 'react-chartjs-2';
-import './chart.css';
+import styled from 'styled-components';
+
+
+
+const DivBackground = styled.div`
+height: 350px;
+width:350px;
+background-color:rgba(0, 15, 30, 0.85);
+text-align:center;
+padding: 10px;
+margin-top: 15px;
+margin-bottom: 15px
+`
+const ChartDiv=styled.div`
+display:flex;
+align-content:flex-end;
+`
 
 
 
 const data={
     labels:["14:00","15:00","16:00","17:00"],
-    datasets:[
-        {label: "SUM change",
-        data:[6850,6950,6450,6788]
+    datasets: [{
+        fill: true,
+        responsive: true,
+          lineTension: 0.5,
+          backgroundColor: 'rgba(0, 15, 30, 0.95)',
+          borderColor: 'gradient',
+          pointBorderColor: '#111',
+          pointBackgroundColor: 'rgba(223, 249, 255, 1)',
+          pointBorderWidth: 2,
+        backgroundColor: 'rgba(251, 59, 108, 0.65)',
+        data: [
+          '',437, 1387, 2298, 5324, 13811, 28508, 34014
+        ]
+      }],
+      
     }
-    ],
-
-}
+    
 
 class ChartSum extends React.Component {
 
@@ -32,16 +58,15 @@ class ChartSum extends React.Component {
             chart: 0
         }
     }
-
+ 
     render(){
 
         return(
-            <div> 
-            <h3>SUM of Portfolio Displayed Here</h3>
-           <div className="chart"> 
+            <DivBackground> 
+           <ChartDiv> 
             <Line data={data}/>
-            </div>
-            </div>
+            </ChartDiv>
+            </DivBackground>
 
         )
     }
--- src/Components/portfolio/Portfolio.js
+++ src/Components/portfolio/Portfolio.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
 import CoinContainer from '../coin-container/CoinContainer';
 import ChartSum from './Chart';
+import styled from 'styled-components';
+
+//CSS
+
+const ButtonMini= styled.button `
+font-family: "Space Mono";
+font-weight: bold;
+background-color:rgba(183, 207, 214, 1);
+font-size: 0.9rem;
+width: 100px;
+height: 30px;
+text-align: center;
+`
+
+
 
 
 function Portfolio(props) {
@@ -38,10 +53,10 @@ function Portfolio(props) {
             </div>
             <ChartSum />
             <div className="changeButtons">
-                <button>1h</button>
-                <button>6h</button>
-                <button>1d</button>
-                <button>7d</button>
+                <ButtonMini>1h</ButtonMini>
+                <ButtonMini>6h</ButtonMini>
+                <ButtonMini>1d</ButtonMini>
+                <ButtonMini>7d</ButtonMini>
             </div>
             <CoinContainer portfolio={props.portfolio} inPortfolio={true} />
         </div>
